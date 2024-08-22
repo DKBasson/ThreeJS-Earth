af09cd1e576c791c81e7c8aedff5f7cd46ea5941
Deklerk Basson <deklerk@dkbasson.com>
2024-08-22 20:48:06 +0200

    Trying to fix Build and Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'vite-plugin-resolve';
 
 export default defineConfig({
-  base: '/ThreeJS-Earth/',
-  plugins: [
-    resolve({
-      'three': 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js'
-    })
-  ]
+  base: '/ThreeJS-Earth/', 
 });
