9e62ee4171ed7168b6148f64a6b4b52cc42cce21
Deklerk Basson <deklerk@dkbasson.com>
2024-08-22 20:30:39 +0200

    Adding Vite Plugin Resolve

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'vite-plugin-resolve';
 
 export default defineConfig({
   base: '/ThreeJS-Earth/',
+  plugins: [
+    resolve({
+      'three': 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js'
+    })
+  ]
 });
-
